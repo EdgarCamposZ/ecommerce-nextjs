21be3396290beb5746c19a6dbb9f868c1d33f218
Edgar Campos <Camposedgar899@gmail.com>
2023-10-12 23:34:25 -0600

    Creacion del Sidebar y Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +19,18 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-[#dddddd] dark:bg-[#313138]")}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={true}
+            storageKey='theme-lms'
+          >
+            {children}
+          </ThemeProvider></body>
       </html>
     </ClerkProvider>
   )
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         warning: "bg-yellow-500 text-black hover:bg-yellow-700",
+        customghost: "hover:bg-teal-600 hover:text-white hover:dark:bg-yellow-500 hover:dark:text-black",
       },
       size: {
         default: "h-10 px-4 py-2",
