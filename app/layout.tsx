5b8fb76aa196f613d88c9debfe8cd0cb8aa0a993
Edgar Campos <Camposedgar899@gmail.com>
2023-10-12 23:43:10 -0600

    Cursos 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ToasterProvider } from '@/components/providers/toaster-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -29,9 +30,12 @@ export default function RootLayout({
             enableSystem={true}
             storageKey='theme-lms'
           >
+            <ToasterProvider />
             {children}
-          </ThemeProvider></body>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
 }
+
