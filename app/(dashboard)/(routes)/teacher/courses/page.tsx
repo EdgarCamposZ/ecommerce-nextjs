9c802bb2cbccc101afc5e5417074bc485d893059
Edgar Campos <Camposedgar899@gmail.com>
2023-11-14 16:05:24 -0600

    Creacion del data-table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.tbl_cursos.findMany({
+        where: {
+            id_usuario: userId
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            categoria: true
+        }
+    });
+
     return (
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button> Crear curso </Button>
-            </Link>
+            <div className="mt-6 border-2 bg-[#cfcfcf] dark:bg-[#1f1f1f] rounded-md p-4 border-solid">
+                <DataTable columns={columns} data={courses} />
+            </div>
         </div>
     );
 }
